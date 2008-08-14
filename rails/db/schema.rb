56b70bafb27c250d3ca8dd1890b7c85b5c2f0c22
Matthew Campbell <mcampbell@hyperworks.nu>
2008-08-13 21:15:08 -0400

    epa data

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -1,12 +1,18 @@
 ActiveRecord::Schema.define do
 
    create_table :epa do |t|
-     t.string   :year
+     t.string   :class
      t.string   :mfr
      t.string   :car_line
+     t.string   :displacement
+     t.string   :cylnder
+     t.string    :transmission
+     t.string    :drive_sys
      t.integer   :city_mpg
      t.integer   :hwy_mpg
      t.integer  :comb_mpg
+     t.string   :fuel_type
+     t.string   :year
      t.timestamps
    end
  
